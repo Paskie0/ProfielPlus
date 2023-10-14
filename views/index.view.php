b6748d3142ed3fc48b4844414c40cb1dc44bc6cc
RABroere <broere.robin@gmail.com>
2023-10-14 11:07:08 +0200

    added private.php and basic html

--- views/index.view.php
+++ views/index.view.php
@@ -1,1 +1,10 @@
-<?php
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ProfielPlus</title>
+</head>
+<body>
+<span>test</span>
+</body>
+</html>
