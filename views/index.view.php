a9b33ca0f235fa3ec6d45ea8f37d20be77dc4b89
RABroere <broere.robin@gmail.com>
2023-10-14 11:25:07 +0200

    added partials folder + files

--- views/index.view.php
+++ views/index.view.php
@@ -1,10 +1,18 @@
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>ProfielPlus</title>
 </head>
 <body>
-<span>test</span>
+<header>
+    <?php require 'partials/header.php' ?>
+</header>
+<main>
+
+</main>
+<footer>
+    <?php require 'partials/footer.php' ?>
+</footer>
 </body>
 </html>
