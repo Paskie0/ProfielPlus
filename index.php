62fc35bbd4c23b674c90297bea98c5e5197001a3
Pascal de Wit <Pascaldewit@outlook.com>
2023-10-16 21:23:51 +0200

    Revert "Trying to fix routes" Will fix later

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 $routes = [
-    '/ProfielPlus' => 'controllers/index.php',
-    '/ProfielPlus/account' => 'controllers/account.php',
-    '/ProfielPlus/admin' => 'controllers/admin.php',
-    '/ProfielPlus/portfolio' => 'controllers/portfolio.php'
+    '/' => 'controllers/index.php',
+    '/account' => 'controllers/account.php',
+    '/admin' => 'controllers/admin.php',
+    '/portfolio' => 'controllers/portfolio.php'
 ];
 
 if (array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
     require $routes[$_SERVER['REQUEST_URI']];
 } else {
-    header("Location: /ProfielPlus");
+    header("Location: /");
 }
