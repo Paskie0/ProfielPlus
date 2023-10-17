5740845d2736caa6da40f4689f2d861293fb13bf
Pascal de Wit <Pascaldewit@outlook.com>
2023-10-17 10:37:06 +0200

    Changed controllers url to match root

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 $routes = [
-    '/ProfielPlus' => 'controllers/index.php',
-    '/ProfielPlus/account' => 'controllers/account.php',
-    '/ProfielPlus/admin' => 'controllers/admin.php',
-    '/ProfielPlus/portfolio' => 'controllers/portfolio.php'
+    '/ProfielPlus' => '/ProfielPlus/controllers/index.php',
+    '/ProfielPlus/account' => '/ProfielPlus/controllers/account.php',
+    '/ProfielPlus/admin' => '/ProfielPlus/controllers/admin.php',
+    '/ProfielPlus/portfolio' => '/ProfielPlus/controllers/portfolio.php'
 ];
 
 if (array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
