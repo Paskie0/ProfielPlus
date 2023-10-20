e48acd034b85227f88f4f3a440872b7d8f2a3649
BramKroone <s1188786@student.windesheim.nl>
2023-10-20 18:27:01 +0200

    Added footer and links to linkedin etc.

--- views/partials/footer/footer.php
+++ views/partials/footer/footer.php
@@ -1,3 +1,27 @@
+<link href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css" rel="stylesheet">
 <footer>
-    <div class="center">Copyright © ProfielPlus 2023</div>
+    <div class="column">
+        <div class="logo">
+            <img src="/images/PROFIEL%20PLUS%20inverted.png">
+        </div>
+        <div class="socials">
+            <a href="https://www.linkedin.com/in/paskie/"><i class="ri-linkedin-box-fill"></i></a>
+            <a href="https://www.linkedin.com/in/robin-broere-310a72223/"><i class="ri-linkedin-box-fill"></i></a>
+            <a href="https://www.linkedin.com/in/bram-kroone-010839294/"><i class="ri-linkedin-box-fill"></i></a>
+        </div>
+    </div>
+    <div class="column">
+        <h4>About Us</h4>
+        <a href="https://www.youtube.com/@WindesheimFlevoland">Channel</a>
+        <a href="https://www.youtube.com/watch?v=BorE4UZcWZI">Education</a>
+        <a href="https://www.windesheim.com/">University of Applied Sciences</a>
+    </div>
+    <div class="column">
+        <h4>E-Mail</h4>
+        <a href="mailto:Pascal.de.wit@windesheim.nl">Pascal</a>
+        <a href="mailto:Robin.broere@windesheim.nl">Robin</a>
+        <a href="mailto:Bram.kroone@windesheim.nl">Bram</a>
+    </div>
 </footer>
+
+<div class="copyright">Copyright © ProfielPlus 2023</div>
