34775cf996e0530a08ccecb4b525a5bebbf72e8e
Pascal de Wit <Pascaldewit@outlook.com>
2023-10-28 14:12:51 +0200

    Created portfolio page
    • Created portfolio page skeleton
    • Added basic css to form the layout

--- views/portfolio.view.php
+++ views/portfolio.view.php
@@ -2,12 +2,92 @@
 
 <head>
     <?php require 'partials/head.php' ?>
+    <link rel="stylesheet" href="../views/partials/css/portfolio.css" />
 </head>
 
 <body>
     <?php require 'partials/header/header.php' ?>
     <main>
-
+        <div id="profile">
+            <img src="" alt="picture" id="pfp">
+            <span id="name">Pascal de Wit</span>
+            <span id="title">Student</span>
+            <p id="bio">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div id="skills">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+        <div id="education">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+        <div id="cv">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+        <div id="projects">
+            <div>
+                <span>1</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+            <div>
+                <span>2</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+            <div>
+                <span>3</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+            <div>
+                <span>4</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+            <div>
+                <span>5</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+            <div>
+                <span>6</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+            <div>
+                <span>7</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+            <div>
+                <span>8</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+            <div>
+                <span>9</span>
+                <img src="" alt="">
+                <a href=""></a>
+            </div>
+        </div>
     </main>
     <?php require 'partials/footer/footer.php' ?>
 </body>
