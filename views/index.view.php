f75064fe38399a1423b2ce85e8aed4db4af62036
BramKroone <s1188786@student.windesheim.nl>
2023-11-03 00:31:12 +0100

    index page

--- views/index.view.php
+++ views/index.view.php
@@ -10,38 +10,29 @@
     <?php require 'partials/header/header.php' ?>
     <main>
         <section>
-            <h2>Profiel</h2>
+            <h2>Over Ons</h2>
             <p>
-
+                Welkom op onze Portfolio Applicatie<br>
+                <br>Wij, Pascal, Robin en Bram, zijn studenten aan de Hogeschool Windesheim Flevoland. Voor meer inhoud over
+                het project kunt u hiernaast lezen. Mocht u meer willen weten over ons dan verwijzen wij naar onze gelinkte
+                LinkedIns die u onderaan de pagina terug kunt vinden. Ook is het mogelijk om ons een mail te schieten met
+                eventuele vragen of opmerkingen over ons project.
             </p>
         </section>
         <section2>
-            <h2>Ontdek Ook</h2>
+            <h2>Het Project</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Het project dat wij moesten maken was een Portfolio Applicatie waarbij de gebruikers zich gemakkelijk
+                    konden verplaatsen over de website en wij moesten er dus voor zorgen dat we een overzichtelijke User Interface maakten.
+                    Daarnaast moesten de gebruikers kunnen in- en uitloggen en in hun account een aantal dingen kunnen toevoegen aan
+                    het profiel, denk aan een bio, een educatie, werk wat je eventueel heb gehad, projecten waar je mee bezig bent
+                    en ook nog vaardigheden die jij bezit. Deze toevoegingen moesten opgeslagen worden in een sql database en de
+                    veranderingen moesten weer getoond werden op het scherm, naast dat je dingen moet kunnen toevoegen moest je ook dingen kunnen
+                    verwijderen omdat je vaardigheden bijvoorbeeld veranderen over de tijd of omdat je bij een andere werkgever aan de slag gaat<br> Bij
+                    dit project werden we helemaal losgelaten en konden we zelf kiezen wat voor kleurschema's layouts en lettertypes we
+                    gingen gebruiken. Dit hebben we met onze groep overlegd en zijn uiteindelijk
+                    gekomen op een redelijk neutrale marine kleur met wat groene tinten erbij zodat het niet een hele drukke indruk geeft, maar
+                    het toch een mooie combinatie is.
                 </p>
         </section2>
     </main>
